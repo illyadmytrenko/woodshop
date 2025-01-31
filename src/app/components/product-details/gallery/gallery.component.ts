a3a9f4339b158292e6660a397666251a0fd86bb7
ivankondratov-hacken <i.kondratov@hacken.io>
2025-01-31 12:19:13 +0200

    first commit

---
+++ src/app/components/product-details/gallery/gallery.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input } from '@angular/core';
+import { Product } from '../../../models/product.model';
+
+@Component({
+  selector: 'app-gallery',
+  templateUrl: './gallery.component.html',
+  styleUrls: ['./gallery.component.css'],
+})
+export class GalleryComponent {
+  @Input() product: Product|null =null;
+  selectedImage: string | undefined = this?.product?.productImagesBas64[1];
+
+  // selectedImage: string = '/assets/img/product_sample.png';
+
+  changeImage(image: string) {
+    this.selectedImage = image;
+  }
+}
